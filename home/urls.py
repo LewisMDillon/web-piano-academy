9e6a9d9c767eec542be97d998aeb6db13c86b2f5
LewisMDillon <lewismdillon1@gmail.com>
2023-10-10 13:51:06 +0000

    Add links & styling to navbar

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('faqs/', views.faqs, name='faqs'),
+    path('courses/', views.courses, name='courses'),
+    path('about/', views.about, name='about'),
 ]
