38847ccf4195437ad9990eb7a732eed1d6669486
LewisMDillon <lewismdillon1@gmail.com>
2023-10-11 11:49:09 +0000

    Create products app and product & cateogry models

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     os.getenv("CURRENT_HOST"),
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'storages',
     'home',
+    'products',
     
 ]
 
