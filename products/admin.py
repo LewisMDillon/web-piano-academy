ac4feec31c6e291e591e53929bab6b5922fab327
LewisMDillon <lewismdillon1@gmail.com>
2023-10-13 16:32:10 +0000

    Add courses to our-courses page

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,15 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'date_created'
+    )
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
