269dfe04aae6fdc16441369eb8de1e82bf7e5266
LewisMDillon <lewismdillon1@gmail.com>
2023-10-16 13:27:35 +0000

    Add category filters to product page & navbar

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,20 +10,34 @@ def courses(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Please enter your search request in the box provided")
+                messages.error(
+                    request,
+                    "Please enter your search request in the box provided"
+                    )
                 return redirect(reverse('courses'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = (
+                Q(name__icontains=query) |
+                Q(description__icontains=query) |
+                Q(level__icontains=query)
+                )
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
     
     return render(request, 'products/courses.html', context)
