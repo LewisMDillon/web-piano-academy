7208245aecf2761a963f1d0dfc8cc5cc41914cb3
LewisMDillon <lewismdillon1@gmail.com>
2023-10-19 13:02:30 +0000

    Fix error in checkout model price calculation

--- checkout/models.py
+++ checkout/models.py
@@ -39,15 +39,8 @@ class Order(models.Model):
         """
         # I messed with the line spacing on this so it
         # might not work
-        self.order_total = self.lineitems.aggregate(
-            (Sum('lineitem_total'))['lineitem_total__sum']
-            )
-        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = (
-                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-                )
-        else:
-            self.delivery_cost = 0
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
