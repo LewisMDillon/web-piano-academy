743a7d9e305590b17d7b0d93345e24562ce852c3
LewisMDillon <lewismdillon1@gmail.com>
2023-10-19 12:36:58 +0000

    Add basic checkout functionality with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,45 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "You don't have anything in your basket!")
         return redirect(reverse('courses'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            'Stripe public key is missing. check your environment vars'
+            )
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51O2unyAYWAqORA4GchES5VgHwTGtpJaG82T5dqrigzzdDh4GVS03fDRqZFB02jq43UyMib5fX2Rtt9BAC0FB71vY00DdUi1Yxg'
-        
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+
     }
 
     return render(request, template, context)
--- config/settings.py
+++ config/settings.py
@@ -194,3 +194,9 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
