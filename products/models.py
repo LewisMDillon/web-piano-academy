03002342d447dae52000153633bad083c46ec4b5
LewisMDillon <lewismdillon1@gmail.com>
2023-10-23 13:12:09 +0000

    Product model - remove sku, add course_url

--- products/models.py
+++ products/models.py
@@ -22,7 +22,6 @@ class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL
         )
-    sku = models.CharField(max_length=254, null=True, blank=True)
     level = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -31,6 +30,7 @@ class Product(models.Model):
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True
         )
+    course_url = models.URLField(max_length=1024, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
