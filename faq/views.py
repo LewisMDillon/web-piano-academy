a64216a907d25be1848d55a82339f7a1e55e59e8
LewisMDillon <lewismdillon1@gmail.com>
2023-10-23 11:34:04 +0000

    Add functionality to display faqs in template

--- faq/views.py
+++ faq/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Faq
 # Create your views here.
 
 
 def faq(request):
-    return render(request, 'faq/faq.html')
+
+    context = {
+        'faqs': Faq.objects.all(),
+    }
+    
+    return render(request, 'faq/faq.html', context)
