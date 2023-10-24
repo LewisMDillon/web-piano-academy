164b764cf563e8b5d0514d0c491338ff563ceeae
LewisMDillon <lewismdillon1@gmail.com>
2023-10-24 11:09:02 +0000

    Add user profile creation to webhook

--- checkout/models.py
+++ checkout/models.py
@@ -102,4 +102,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'Name: {self.product.name} -- Order: {self.order.order_number}'
