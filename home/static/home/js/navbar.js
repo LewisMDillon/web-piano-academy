b82499d0dc860415ae08109257382836a0a27e0a
LewisMDillon <lewismdillon1@gmail.com>
2023-10-26 16:48:39 +0000

    Add background transition to navbar

---
+++ home/static/home/js/navbar.js
@@ -0,0 +1,14 @@
+console.log("The script is online!!!")
+
+document.onreadystatechange = function() {
+    let lastScrollPosition = 0;
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function(e) {
+      lastScrollPosition = window.scrollY;
+      
+      if (lastScrollPosition > 100)
+        navbar.classList.add('navbar-dark');
+      else
+        navbar.classList.remove('navbar-dark');
+    });
+  }
