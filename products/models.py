67eb2c40ba5b3e05a5d1a85ff87d1e1e80c78d0b
LewisMDillon <lewismdillon1@gmail.com>
2023-10-27 14:52:32 +0000

    Edit styling on allauth templates

--- products/models.py
+++ products/models.py
@@ -28,8 +28,12 @@ class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL
         )
-    level = models.CharField(choices=LEVEL_CHOICES, max_length=254, null=True, blank=True)
-    level_display_name = models.CharField(max_length=254, null=True, blank=True)
+    level = models.CharField(
+        choices=LEVEL_CHOICES, max_length=254, null=True, blank=True
+        )
+    level_display_name = models.CharField(
+        max_length=254, null=True, blank=True
+        )
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
