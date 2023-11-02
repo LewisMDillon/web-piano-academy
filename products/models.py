7b82f18de7477ac79bf07d9abf9f927d39a9b0b5
LewisMDillon <lewismdillon1@gmail.com>
2023-11-02 16:40:33 +0000

    Remove rating field from product model

--- products/models.py
+++ products/models.py
@@ -39,9 +39,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     date_created = models.DateTimeField(default=timezone.now)
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True
-        )
     course_url = models.URLField(max_length=1024, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
