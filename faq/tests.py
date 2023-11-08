49a837a4f5ec26a6847333c88ac88d793299c567
LewisMDillon <lewismdillon1@gmail.com>
2023-11-08 14:05:08 +0000

    Add automated tests for faq app views & model

--- faq/tests.py
+++ faq/tests.py
@@ -1,3 +1,66 @@
 from django.test import TestCase
+from .models import Faq
 
-# Create your tests here.
+# ------------ MODEL TESTING ------------
+
+class TestFaq(TestCase):
+    """Tests the faq model in the faq app."""
+    def setUp(self):
+        """
+        Makes a sample Faq object
+        """
+
+        question = "test_question"
+        answer = "test_answer"
+        faq1 = Faq.objects.create(
+            question=question,
+            answer=answer,
+            )
+
+        faq1.save()
+
+    def test_str(self):
+        """Tests the string method on the post."""
+        # Retrieves the most recently created faq and gets its string
+        faq1 = Faq.objects.latest('pk')
+        faq_string_question = str(faq1.question)
+
+        # Cofirms the faq string is correct.
+        self.assertEqual((faq_string_question), (faq1.question))
+
+
+# ------------ VIEWS TESTING ------------
+
+class FaqViewTestCase(TestCase):
+    """
+    Test case for testing Faq views.
+    """
+
+    def setUp(self):
+        """
+        Makes a sample Faq object
+        """
+
+        question = "test_question"
+        answer = "test_answer"
+        faq1 = Faq.objects.create(
+            question=question,
+            answer=answer,
+            )
+
+        faq1.save()
+
+
+    def test_faq_render_context(self):
+        """
+        Tests that the faq page is rendered properly and all of
+        the correct context is passed into the page
+        """
+
+        response = self.client.get('/faq/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'faq/faq.html', 'base.html'
+            )
+        # Checks whether the context item is passed in
+        self.assertTrue(response.context['faqs'])
