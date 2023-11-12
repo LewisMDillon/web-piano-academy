c965041c3620a3d3f1d6ce61a9e803823e9cdfd2
LewisMDillon <lewismdillon1@gmail.com>
2023-11-12 18:57:47 +0000

    Add urls & views to test custom error pages

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
+    path('error400/', views.errortest400, name='errortest400'),
+    path('error403/', views.errortest403, name='errortest403'),
+    path('error404/', views.errortest404, name='errortest404'),
+    path('error500/', views.errortest500, name='errortest500'),
 ]
