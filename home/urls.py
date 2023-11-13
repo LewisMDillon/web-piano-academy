4f963064b0c3ebd96a8c6aa6019cde76ba18df7f
LewisMDillon <lewismdillon1@gmail.com>
2023-11-13 00:12:08 +0000

    Delete test error page urls & views

--- home/urls.py
+++ home/urls.py
@@ -5,8 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('error400/', views.errortest400, name='errortest400'),
-    path('error403/', views.errortest403, name='errortest403'),
-    path('error404/', views.errortest404, name='errortest404'),
-    path('error500/', views.errortest500, name='errortest500'),
 ]
--- home/views.py
+++ home/views.py
@@ -9,19 +9,3 @@ def index(request):
 
 def about(request):
     return render(request, 'home/about.html')
-
-
-def errortest400(request):
-    return render(request, '400.html')
-
-
-def errortest403(request):
-    return render(request, '403.html')
-
-
-def errortest404(request):
-    return render(request, '404.html')
-
-
-def errortest500(request):
-    return render(request, '500.html')
